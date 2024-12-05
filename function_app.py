449747f42ad7dd767dee2e01dd712b01849a0c19
qimeng666 <77719606+qimeng666@users.noreply.github.com>
2024-12-04 22:59:59 -0600

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -4,8 +4,10 @@ import json
 import logging
 from deepface import DeepFace
 import tempfile
+import requests
 
 app = func.FunctionApp()
+url = "https://feelxpert.azurewebsites.net/api/upload_image?"
 
 
 @app.blob_trigger(arg_name="myblob", path="qimengimage",
@@ -25,6 +27,26 @@ def AnalyzeImages(myblob: func.InputStream):
 
         # Log the analysis result
         logging.info(f"Analysis result for {myblob.name}: {analysis_result}")
-
+        logging.info(analysis_result[0]['dominant_emotion'])
+        blob_url = f"https://qimeng.blob.core.windows.net/qimengimage/{myblob.name}"
+        data = {
+            "userid": "80001",
+            "status": "completed",
+            "emotion": analysis_result[0]['dominant_emotion'],
+            "originaldatafile": myblob.name,
+            "datafilekey": blob_url, 
+        }
+        headers = {"Content-Type": "application/json"}
+        try:
+            response = requests.post(url, headers=headers, json=data)
+            response.raise_for_status()
+            logging.info(str(response))
+            if response.status_code == 200:
+                logging.info("Data successfully uploaded")
+            else:
+                logging.info("Unexpected response")
+        except requests.exceptions.RequestException as e:
+            logging.info("Error occurred:", e)
     except Exception as e:
         logging.error(f"Error processing blob {myblob.name}: {e}")
+
